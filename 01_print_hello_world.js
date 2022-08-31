a08426f64b96230bd3427319f87fda291ae2c555
Francisco Javier Olivares Diaz <franciscojavier.olivaresd@gmail.com>
2022-08-31 17:40:45 -0400

    work in progress

---
+++ 01_print_hello_world.js
@@ -0,0 +1,12 @@
+// Practice 1: A program to print hello world.
+
+// Practice A. The console method.
+console.log('Hello World');
+
+
+// Practice B. The alert method.
+alert('Hello World');
+
+
+// Practice C. The document menthod.
+document.write('Hello World');
---
+++ 02_add_two_numbers.js
@@ -0,0 +1,17 @@
+// Practice 2: A program to add two numbers.
+
+// Practice A: Let's add two numbers only.
+const num1 = 8;
+const num2 = 2;
+// addition of two numbers
+const sum = num1 + num2;
+// display the output
+console.log('The sum of' + num1 + 'and' + num2 + '=' + sum);
+
+
+
+// Practice B: Sum two numbers based on user input.
+const numOne = parseInt(prompt('Enter 1st number:'));
+const numTwo = parseInt(prompt('Enter 2do number:'));
+
+
